1cfce21dac5b41ef9047070dd07fbd2e26882911
hereisSwapnil <swapnilskumars@gmail.com>
2024-05-22 00:18:56 +0530

    fixed

--- model/main.py
+++ model/main.py
@@ -20,7 +20,11 @@ app = FastAPI()
 origins = ["http://localhost:3000/"]
 
 app.add_middleware(
-    CORSMiddleware, allow_origins=origins, allow_methods=["*"], allow_headers=["*"]
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["*"],
 )
 
 @app.get("/")
