88543bcf0690a0a867dd3af494e722f49de9c423
hereisSwapnil <swapnilskumars@gmail.com>
2024-05-22 00:37:25 +0530

    fix

--- model/main.py
+++ model/main.py
@@ -17,19 +17,10 @@ analysis = IndustryAnalysis(api_key, grok_model, news_api_key)
 
 app = FastAPI()
 
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost:3000",
-    "http://localhost:8080",
-]
+origins = ["https://localhost:3000"]
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["GET", "POST"],  # Allow only the required methods
-    allow_headers=["*"],  # Allow all headers for simplicity, you can restrict as needed
+    CORSMiddleware, allow_origins=origins, allow_methods=["*"], allow_headers=["*"]
 )
 
 @app.get("/")
